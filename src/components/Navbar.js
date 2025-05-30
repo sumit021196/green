fec6951f5a1908f5bd81719e4dfb804e2965d3b4
sumit021196 <154895578+sumit021196@users.noreply.github.com>
2025-05-30 15:35:37 +0530

    ok

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -57,19 +57,44 @@ function Navbar() {
             <li><Link to="/investor/disclosure" onClick={() => setIsMenuOpen(false)}>Stock Exchange Disclosure</Link></li>
             <li><Link to="/investor/grievance" onClick={() => setIsMenuOpen(false)}>Investor Grievance</Link></li>
             <li className="has-submenu">
-              <Link to="/investor/code-of-conduct" onClick={() => setIsMenuOpen(false)} className="submenu-link">
+              <Link to="/investor/code-of-conduct" onClick={handleDropdownClick} className="submenu-link">
                 Code Of Conduct
                 <span className="submenu-arrow">&#8250;</span>
               </Link>
-              {/* Submenu can be implemented here */}
+              <ul className="submenu">
+                <li><Link to="/investor/code-of-conduct/sebi-lodr" onClick={() => setIsMenuOpen(false)}>Regulation 7 (5) SEBI (LODR) Regulations,2015</Link></li>
+                <li><Link to="/investor/code-of-conduct/insider-trading" onClick={() => setIsMenuOpen(false)}>Insider trading code</Link></li>
+              </ul>
             </li>
-            <li><Link to="/investor/statutory-document" onClick={() => setIsMenuOpen(false)}>Statutory Document</Link></li>
             <li className="has-submenu">
-              <Link to="/investor/shareholders-help-desk" onClick={() => setIsMenuOpen(false)} className="submenu-link">
+              <Link to="/investor/statutory-document" onClick={handleDropdownClick} className="submenu-link">
+                Statutory Document
+                <span className="submenu-arrow">&#8250;</span>
+              </Link>
+              <ul className="submenu">
+                <li><Link to="/investor/statutory-document/annual-reports" onClick={() => setIsMenuOpen(false)}>Annual Reports</Link></li>
+                <li><Link to="/investor/statutory-document/offer-documents" onClick={() => setIsMenuOpen(false)}>Offer Documents</Link></li>
+              </ul>
+            </li>
+            <li className="has-submenu">
+              <Link to="/investor/shareholders-help-desk" onClick={handleDropdownClick} className="submenu-link">
                 Shareholder's Help Desk
                 <span className="submenu-arrow">&#8250;</span>
               </Link>
-              {/* Submenu can be implemented here */}
+              <ul className="submenu">
+                <li><Link to="/investor/shareholders-help-desk/postal-ballot" onClick={() => setIsMenuOpen(false)}>Postal Ballot</Link></li>
+                <li><Link to="/investor/shareholders-help-desk/agm-egm-notice" onClick={() => setIsMenuOpen(false)}>AGM / EGM Notice</Link></li>
+                <li><Link to="/investor/shareholders-help-desk/book-closure" onClick={() => setIsMenuOpen(false)}>Book Closure Notice</Link></li>
+                <li><Link to="/investor/shareholders-help-desk/nomination-form" onClick={() => setIsMenuOpen(false)}>Nomination Form</Link></li>
+                <li><Link to="/investor/shareholders-help-desk/scrutinizers-reports" onClick={() => setIsMenuOpen(false)}>Scrutinizer's Reports</Link></li>
+                <li><Link to="/investor/shareholders-help-desk/unclaimed-dividend" onClick={() => setIsMenuOpen(false)}>Unclaimed Dividend</Link></li>
+                <li><Link to="/investor/shareholders-help-desk/dividend-history" onClick={() => setIsMenuOpen(false)}>Dividend History</Link></li>
+                <li><Link to="/investor/shareholders-help-desk/investor-presentations" onClick={() => setIsMenuOpen(false)}>Investor Presentations</Link></li>
+                <li><Link to="/investor/shareholders-help-desk/familiarization-programme" onClick={() => setIsMenuOpen(false)}>Familiarization Programme For Ujaas</Link></li>
+                <li><Link to="/investor/shareholders-help-desk/investor-relations" onClick={() => setIsMenuOpen(false)}>Investor Relations</Link></li>
+                <li><Link to="/investor/shareholders-help-desk/independent-director" onClick={() => setIsMenuOpen(false)}>Terms and Conditions for the Appointment of Independent Director</Link></li>
+                <li><Link to="/investor/shareholders-help-desk/annual-return" onClick={() => setIsMenuOpen(false)}>Annual Return</Link></li>
+              </ul>
             </li>
             <li><Link to="/investor/policies" onClick={() => setIsMenuOpen(false)}>Policies</Link></li>
             <li><Link to="/investor/corporate-governance" onClick={() => setIsMenuOpen(false)}>Corporate Governance</Link></li>
