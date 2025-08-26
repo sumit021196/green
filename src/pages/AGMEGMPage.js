cbec4043a36f04f3a4ce48e493e90be5ef02c4c2
sumit021196 <154895578+sumit021196@users.noreply.github.com>
2025-08-26 16:56:28 +0530

    AGM notice

--- src/pages/AGMEGMPage.js
+++ src/pages/AGMEGMPage.js
@@ -4,12 +4,30 @@ import '../App.css';
 import './AGMEGMPage.css';
 
 function AGMEGMPage() {
+  // Function to handle the download
+  const handleDownload = (e) => {
+    e.preventDefault();
+    const link = document.createElement('a');
+    link.href = '/pdf/Notice of 1st AGM_Globlegreen Power Limited.pdf';
+    link.download = 'Notice of 1st AGM_Globlegreen Power Limited.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div className="agm-egm-page">
       <h1>AGM / EGM Notices</h1>
       <div className="notice-list">
         <div className="notice-item">
-          <Link to="#" className="notice-title">First AGM Notice</Link>
+          <a 
+            href="/pdf/Notice of 1 st.pdf" 
+            className="notice-title" 
+            onClick={handleDownload}
+            download
+          >
+            Notice of 1st AGM
+          </a>
         </div>
       </div>
     </div>
