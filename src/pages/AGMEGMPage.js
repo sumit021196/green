bfa2416400cf2ef3befcffcec28cbd919733ae1e
sumit021196 <154895578+sumit021196@users.noreply.github.com>
2025-08-26 17:25:25 +0530

    ok

--- src/pages/AGMEGMPage.js
+++ src/pages/AGMEGMPage.js
@@ -4,35 +4,15 @@ import '../App.css';
 import './AGMEGMPage.css';
 
 function AGMEGMPage() {
-  // Function to handle the download
-  const handleDownload = (e) => {
-    e.preventDefault();
-    const link = document.createElement('a');
-    link.href = process.env.PUBLIC_URL + '/pdf/Notice of 1st AGM_Globlegreen Power Limited.pdf';
-    link.download = 'Notice of 1st AGM_Globlegreen Power Limited.pdf';
-    link.target = '_blank';
-    link.rel = 'noopener noreferrer';
-    
-    // For mobile devices
-    if (document.createEvent) {
-      const event = document.createEvent('MouseEvents');
-      event.initEvent('click', true, true);
-      link.dispatchEvent(event);
-    } else {
-      link.click();
-    }
-  };
-
   return (
     <div className="agm-egm-page">
       <h1>AGM / EGM Notices</h1>
       <div className="notice-list">
         <div className="notice-item">
           <a 
-            href="/pdf/Notice of 1 st.pdf" 
+            href={`${process.env.PUBLIC_URL}/pdf/Notice of 1st AGM_Globlegreen Power Limited.pdf`}
             className="notice-title" 
-            onClick={handleDownload}
-            download
+            download="Notice of 1st AGM_Globlegreen Power Limited.pdf"
           >
             Notice of 1st AGM
           </a>
