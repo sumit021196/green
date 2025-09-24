59dd1af4fc5fb91572d61eac859df6f53d983c3b
sumit021196 <154895578+sumit021196@users.noreply.github.com>
2025-09-24 17:58:00 +0530

    Update AGMEGMPage.js

--- src/pages/AGMEGMPage.js
+++ src/pages/AGMEGMPage.js
@@ -4,15 +4,17 @@ import '../App.css';
 import './AGMEGMPage.css';
 
 function AGMEGMPage() {
+   const adjournedFileName = 'Notice of adjourned AGM 2024-25 Globlegreen Power Limited.pdf';
+  const adjournedHref = `${process.env.PUBLIC_URL}/pdf/${encodeURIComponent(adjournedFileName)}`;
   return (
     <div className="agm-egm-page">
       <h1>AGM / EGM Notices</h1>
       <div className="notice-list">
         <div className="notice-item">
           <a 
-            href={`${process.env.PUBLIC_URL}/pdf/Notice of adjourned AGM 2024-25 Globlegreen Power Limited.pdf`}
+            href={adjournedHref}
             className="notice-title" 
-            download="Notice of adjourned AGM 2024-25 Globlegreen Power Limited.pdf"
+            download="Notice-of-adjourned-AGM-2024-25-Globlegreen-Power-Limited.pdf"
           >
             Notice of Adjourned AGM 2024-25
           </a>
